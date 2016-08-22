5685fa19f68fb3bdfc65548ee01900e11b2f9b7e
Rob Coffman <rcoffman@redhat.com>
2016-08-21 18:53:11 -0700

    dding refresh script

--- Vagrant/roles/drupal/files/link.sh
+++ Vagrant/roles/drupal/files/link.sh
@@ -2,3 +2,6 @@
 
 rm -rf /var/www/public_html/sites/default/settings.php
 ln -s /var/www/config/dev.settings.php /var/www/public_html/sites/default/settings.php
+
+rm -rf /usr/bin/refresh_db
+ln -s /usr/local/bin/refresh_db.sh /usr/bin/refresh_db
